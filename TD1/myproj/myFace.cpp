c149444c1a09324a9de5a8e81f9076d800415f6e
Lilian Macrez <lilian.macrez@edu.esiee.fr>
2026-04-13 13:38:06 +0200

    implementing OBJ parser and half-edge connectivity (vertices faces next&prev twins)

---
+++ TD1/myproj/myFace.cpp
@@ -0,0 +1,21 @@
+#include "myFace.h"
+#include "myvector3d.h"
+#include "myHalfedge.h"
+#include "myVertex.h"
+#include <GL/glew.h>
+
+myFace::myFace(void)
+{
+	adjacent_halfedge = NULL;
+	normal = new myVector3D(1.0, 1.0, 1.0);
+}
+
+myFace::~myFace(void)
+{
+	if (normal) delete normal;
+}
+
+void myFace::computeNormal()
+{
+/**** TODO ****/
+}
---
+++ TD1/myproj/myVertex.cpp
@@ -0,0 +1,21 @@
+#include "myVertex.h"
+#include "myvector3d.h"
+#include "myHalfedge.h"
+#include "myFace.h"
+
+myVertex::myVertex(void)
+{
+	point = NULL;
+	originof = NULL;
+	normal = new myVector3D(1.0,1.0,1.0);
+}
+
+myVertex::~myVertex(void)
+{
+	if (normal) delete normal;
+}
+
+void myVertex::computeNormal()
+{
+	/**** TODO ****/
+}
